4c529ee776a480b5910f402d6ae18073402c8307
cjredmond <connor.redmond@gmail.com>
2018-02-27 16:10:40 -0500

    finished scaffold

--- searcher.py
+++ searcher.py
@@ -2,7 +2,7 @@ import re
 import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
-from credentials.py import creds
+from credentials import creds
 
 class TwitterClient(object):
 
@@ -17,3 +17,52 @@ class TwitterClient(object):
             self.auth = OAuthHandler(consumer_key, consumer_secret_key)
             self.auth.set_access_token(access_token, access_token_secret)
             self.api = tweepy.API(self.auth)
+
+        except:
+            print("Error: Authentication Failed")
+
+    def clean_tweet(self,tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+|([^0-9A-Za-z \t])|(\w+:\/\/\S+))", " ", tweet).split())
+
+    def get_tweet_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+
+    def get_tweets(self, query, count = 10):
+        tweets = []
+        try:
+            fetched_tweets = self.api.search(q = query, count = count)
+            for tweet in fetched_tweets:
+                parsed_tweet = {}
+                parsed_tweet['text'] = tweet.text
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+
+                if tweet.retweet_count > 0:
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+            return tweets
+
+        except tweepy.TweepError as e:
+            print("Error : " + str(e))
+
+def main():
+    api = TwitterClient()
+    tweets = api.get_tweets(query = 'Aaronnagler', count = 200)
+    for tweet in tweets:
+        print(tweet)
+    print('----------')
+    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+    # print("Neutral tweets percentage: {} %".format(100*len(tweets) / len(tweets)))
+
+
+main()
